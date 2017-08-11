1fb27eddc3ade597654f7a330c142072f2910ac2
Maria Jose Mendez <majomendez88mena@gmail.com>
2017-08-11 16:06:15 -0600

    Todo

--- app.js
+++ app.js
@@ -5,16 +5,87 @@ import {
   View,
   Text,
   StyleSheet,
-  Platform
+  Platform,
+  ListView,
+  Keyboard
 } from 'react-native';
 import Header from "./header";
 import Footer from "./footer";
+import Row from "./row";
 export default class App extends Component {
+
+  constructor(props){
+  	super(props);
+    const ds= new ListView.DataSource({rowHasChanged: (r1, r2) => r1 =! r2});
+  	this.state = {
+      allComplete: false,
+      value: "",
+      items: [],
+      dataSource : ds.cloneWithRows([])
+    }
+    this.setSource = this.setSource.bind(this);
+    this.handleAddItem = this.handleAddItem.bind(this);
+    this.handleToggleAllComplete = this.handleToggleAllComplete.bind(this);
+  }
+  setSource(items, itemsDataSource, otherState = {}){
+    items,
+    dataSource: this.state.dataSource.cloneWithRows(itemsDataSource),
+    ...otherState
+  }
+  handleToggleAllComplete(){
+    const complete = !this.state.allComplete;
+    const newItems = this.state.items.map((item) => ({
+      ...item,
+      complete
+    }))
+     console.table(newItems);
+    this.setState({
+      items : newItems,
+      allComplete: complete
+    })
+  }
+   handleAddItem(){
+     if (!this.state.value) return;
+     const newItems= [
+       ...this.state.items,
+      {
+        key: Date.now(),
+        text: this.state.value,
+        complete:false
+      }
+   ]
+   this.setState({
+     items: newItems,
+     value: ""
+   })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Header/>
+        <Header value={this.state.value}
+        onAddItem={this.handleAddItem}
+        onChange= {(value)=> this.setState({value})}
+        onToggleAllComplete={this.handleToggleAllComplete}
+        />
           <View style={styles.content}>
+          <ListView
+          style={styles.list}
+          enableEmptySections
+          dataSource={this.state.dataSource}
+          onScroll={() => Keyboard.dimiss()}
+          renderRow= {({ key, ...value}) => {
+            return(
+              <Row
+              key = {key}
+              {...value}
+              />
+            )
+          }}
+          renderSeparator= {(sectionId, rowId) =>{
+            return <view key= {rowId} style={styles.separator}/>
+          }}
+          />
           </View>
          <Footer/>
       </View>
@@ -32,5 +103,12 @@ const styles = StyleSheet.create({
   },
   content:{
     flex: 1
+  },
+  separator:{
+    borderWidth: 1,
+    borderColor: "#F5F5F5"
+  },
+  list:{
+    backgroundColor: '#FFF'
   }
 });
--- header.js
+++ header.js
@@ -5,17 +5,25 @@ import {
   View,
   Text,
   StyleSheet,
-  TextInput
+  TextInput,
+  TouchableOpacity
 } from 'react-native';
 
 export default class Header extends Component {
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.header}>
+      <TouchableOpacity onPress= {this.props.onToggleAllComplete}>
+      <Text style={styles.toggleIcon}>{String.fromCharCode(10003)}</Text>
+      </TouchableOpacity>
       <TextInput
-      placeholder"¿Que es lo ahey hay que hacer?"
-      blurOnSubmit={false}
-      returnKeyType "done"
+                 value={this. props.value}
+                 onChangeText={this.props.onChange}
+                 onSubmitEditing={this.props.onAddItem}
+                 placeholder="Que es lo que hay que hacer"
+                 blurOnSubmit={false}
+                 returnKeyType="done"
+                 style={styles.input}
       />
       </View>
     );
@@ -24,10 +32,21 @@ export default class Header extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   input:{
     flex:1,
-    height:50
+    height:50,
+    marginLeft: 16,
+  },
+  toggleIcon:{
+    fontSize: 30,
+    color: "#CCC"
+  },
+  header:{
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
   }
 });
